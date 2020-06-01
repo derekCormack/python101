620c114d2c2838213d5ea52aa0853fa0ad265956
Tait Hoyem <tait@tait.tech>
2020-06-01 14:44:36 +0000

    June 1st

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 test_hello1():
-  print("Hello from play_test")
-    cars = ["Ford", "Volvo", "BMW"]
-    print(cars[0],len(cars))
-    for x in cars:
-            print(x)
-    print('--------------')
-            cars.append("Honda")
-            for x in cars:
+        print("Hello from play_test")
+        cars = ["Ford", "Volvo", "BMW"]
+        print(cars[0],len(cars))
+        for x in cars:
+                print(x)
+        print('--------------')
+        cars.append("Honda")
+        for x in cars:
                 print(x)
 
