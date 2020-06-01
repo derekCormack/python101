60b539a82f00cd0e651de1a630b49913a9f71f64
[derekCormack] <[dcormack.dc@gmail.com]>
2020-06-01 08:13:30 -0600

    first try

---
+++ python101.py
@@ -0,0 +1,41 @@
+# python101
+my exploration and ground up introduction to Python.
+https://www.learnpython.org/
+
+LEARN THE BASIC-------------
+
+Hello, World!
+Variables and Types
+Lists
+Basic Operators
+String Formatting
+Basic String Operations
+Conditions
+Loops
+Functions
+Classes and Objects
+Dictionaries
+Modules and Packages
+   - FLASK,
+   - Beautiful Soup,  (parsing html Docs)
+   - DB, Data visualisation.
+
+DATA SCIENCE TUTORIALS-------------
+
+Numpy Arrays
+Pandas Basics
+
+ADVANCE TUTORIALS---------------
+
+Generators
+List Comprehensions
+Multiple Function Arguments
+Regular Expressions
+Exception Handling
+Sets
+Serialization
+Partial functions
+Code Introspection
+Closures
+Decorators
+Map, Filter, Reduce
